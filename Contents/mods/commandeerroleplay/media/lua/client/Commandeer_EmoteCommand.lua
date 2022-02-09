ddf2b7468cc6286f797acae03ead8bf8757fdf91
Paul Vincent Salvador G. Magbojos <paul.vincent.magbojos@gmail.com>
2022-02-09 15:44:34 +0800

    Removed styling in /me

--- Contents/mods/commandeerroleplay/media/lua/client/Commandeer_EmoteCommand.lua
+++ Contents/mods/commandeerroleplay/media/lua/client/Commandeer_EmoteCommand.lua
@@ -4,7 +4,7 @@ Commandeer_CommandHandler.commands["/me"] = function(param)
 	if not param or param == "" then
 		return "Usage: /me does a thing.";
 	end
-	processSayMessage(string.format("*%s* ** %s %s", "purple", Commandeer_RPName.getRPName(), param))
+	processSayMessage(string.format("%s %s",Commandeer_RPName.getRPName(), param))
 end
 
 Commandeer_CommandHandler.commands["/m"] = Commandeer_CommandHandler.commands["/me"]
