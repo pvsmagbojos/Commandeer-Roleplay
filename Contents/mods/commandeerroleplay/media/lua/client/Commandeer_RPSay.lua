11615bd597e2febfce005ffc9b91912316e7a931
Paul Vincent Salvador G. Magbojos <paul.vincent.magbojos@gmail.com>
2022-02-09 15:43:21 +0800

    Removed styling for /say

--- Contents/mods/commandeerroleplay/media/lua/client/Commandeer_RPSay.lua
+++ Contents/mods/commandeerroleplay/media/lua/client/Commandeer_RPSay.lua
@@ -20,7 +20,7 @@ Commandeer_CommandHandler.commands["/say"] = function(param)
 	if luautils.stringEnds(param, "...") then
 		verb = "mutters";
 	end
-	processSayMessage(string.format("*%s*%s %s, \"%s\"", "177,210,187", Commandeer_RPName.getRPName(), verb, param))
+	processSayMessage(string.format("%s %s, \"%s\"", Commandeer_RPName.getRPName(), verb, param))
 end
 
 Commandeer_CommandHandler.commands["/s"] = Commandeer_CommandHandler.commands["/say"]
